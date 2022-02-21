d1b0626736eb19c3f5c28153017468c01203e282
Zhengnan Zhao <zzhengnan@protonmail.com>
2022-02-21 12:29:54 -0500

    Heapsort

--- sorting.py
+++ sorting.py
@@ -42,3 +42,61 @@ def quicksort(array, start=None, end=None):
     quicksort(array, right + 1, end)
 
     return array
+
+
+def heapsort(array):
+    """Heapsort.
+
+    Steps
+      1. Build a max heap out of original array
+      2. Conceptualize original array as consisting of two subarrays where
+        - Left subarray is unsorted
+        - Right subarray is sorted
+        - At start of algorithm, left subarray is original array and right subarray is empty
+      3. Swap first and last elements in left subarray, effectively expanding right subarray
+         and shrinking left subarray by 1
+      4. Sift down first element in left subarray in order to maintain heap property
+
+               Time          Space
+    Average    n * log(n)    1
+    Best       n * log(n)    1
+    Worse      n * log(n)    1
+
+    Best case time complexity doesn't include special case where original array contains all dupes,
+    in which case time complexity is O(n).
+    """
+    def build_max_heap(array):
+        parent_idx = (len(array) - 2) // 2  # Last parent node with a child
+        while parent_idx >= 0:
+            sift_down(array, parent_idx)
+            parent_idx -= 1
+
+    def sift_down(array, parent_idx=0, end=None):
+        if end is None:
+            end = len(array) - 1
+
+        left_idx = 2 * parent_idx + 1
+        while left_idx <= end:
+            if left_idx == end:
+                # Only left child exists; swap parent with left if appropriate
+                if array[parent_idx] >= array[left_idx]:
+                    break
+                array[parent_idx], array[left_idx] = array[left_idx], array[parent_idx]
+                parent_idx = left_idx
+            else:
+                # Both children exist; swap parent with max(left, right) if appropriate
+                right_idx = left_idx + 1
+                if array[parent_idx] >= array[left_idx] and array[parent_idx] >= array[right_idx]:
+                    break
+                bigger = left_idx if array[left_idx] >= array[right_idx] else right_idx
+                array[parent_idx], array[bigger] = array[bigger], array[parent_idx]
+                parent_idx = bigger
+            left_idx = parent_idx * 2 + 1
+
+    build_max_heap(array)
+    boundary = len(array) - 1
+    while boundary > 0:
+        array[0], array[boundary] = array[boundary], array[0]
+        boundary -= 1
+        sift_down(array, end=boundary)
+    return array
